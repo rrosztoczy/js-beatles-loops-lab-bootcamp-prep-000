397db0c04b3f9d4318daa934ab978abafb36e585
Ryan Rosztoczy <rrosztoczy@gmail.com>
2018-12-05 04:09:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  var i;
+  var fullArray = [];
+  for (i = 0; i < musicians.length; i++) {
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+    return emptyArray;
+}
+
+function johnLennonFacts (facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts.splice( i, 0, '!!!')
+    i++;
+    return facts;
+  }
+  return facts;
+}
